5c0385b7001dac1d64a887e1b7713bc775b90a9d
michaelsoares91thecawe <michael.soares91@icloud.com>
2025-12-14 15:51:13 +0100

    Debug Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './src/auth.config';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export default NextAuth(authConfig).auth;
+export function middleware(request: NextRequest) {
+    return NextResponse.next();
+}
 
 export const config = {
-    // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
